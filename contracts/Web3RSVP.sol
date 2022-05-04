6316cead9bd290829a14d68e66a43e3a9014ea1c
Sarah Schwartz <sarah.schwartz93@gmail.com>
2022-05-04 14:35:47 -0700

    fixes and comments

--- contracts/Web3RSVP.sol
+++ contracts/Web3RSVP.sol
@@ -6,30 +6,29 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
-//@tod0: add function that "checks people in" to confirm that rsvp'ers actually showed up
-//function to payout people who checked in and burn ppl who didn't
-//
+// to do: figure out how to push to confirmedRSVPs / claimedRSVPs 
 
 contract MyToken is ERC721, Pausable, Ownable, ERC721Burnable {
     address payable owner;
 
+    event newEventCreated(
+        bytes32 eventID,
+        address creatorAddress,
+        uint256 eventTimestamp,
+        uint256 maxCapacity,
+        uint256 deposit
+    );
 
-    event newEventCreated(uint256 eventID, address creatorAddress, uint eventTimestamp, uint256 maxCapacity, uint256 deposit);
+    event newRSVP(bytes32 eventID, address attendeeAddress);
 
-    event newRSVP(uint256 eventID, address attendeeAddress, uint256 rsvpID);
-
-    event confirmedAttendee(
-        uint256 eventID,
-        address attendeeAddress
-        );
-
+    event confirmedAttendee(bytes32 eventID, address attendeeAddress);
 
     constructor() ERC721("Web3RSVP", "W3RSVP") {
         owner = payable(msg.sender);
     }
 
     struct CreateEvent {
-        uint256 eventId;
+        bytes32 eventId;
         address eventOwner;
         uint256 eventTimestamp;
         uint256 deposit;
@@ -39,150 +38,146 @@ contract MyToken is ERC721, Pausable, Ownable, ERC721Burnable {
         bool paidOut;
     }
 
-
-    CreateEvent public createevent; 
-    mapping(uint256 => CreateEvent ) public idToEvent;
+    CreateEvent public createevent;
+    mapping(bytes32 => CreateEvent) public idToEvent;
 
     function createNewEvent(
         uint256 eventTimestamp,
         uint256 deposit,
         uint256 maxCapacity
-    ) {
+    ) public {
         // generate an eventID based on other things passed in to generate a hash
-        bytes32 eventId = keccak256(abi.encodePacked(msg.sender, address(this), eventTimestamp, costPerAttendee, eventName));
+        bytes32 eventId = keccak256(
+            abi.encodePacked(
+                msg.sender,
+                address(this),
+                eventTimestamp,
+                deposit,
+                maxCapacity
+            )
+        );
 
-        //what does this chunk do exactly?
-        //this creates a new CreateEvent struct and adds it to t
-       idToEvent[eventId] = CreateEvent(eventId, msg.sender, eventTimestamp, deposit, maxCapacity, [], [], false);
+        address[] memory confirmedRSVPs;
+        address[] memory claimedRSVPs;
 
-      emit newEventCreated( eventId, msg.sender, eventTimestamp, maxCapacity, deposit);
+        //this creates a new CreateEvent struct and adds it to the idToEvent mapping
+        idToEvent[eventId] = CreateEvent(
+            eventId,
+            msg.sender,
+            eventTimestamp,
+            deposit,
+            maxCapacity,
+            confirmedRSVPs,
+            claimedRSVPs,
+            false
+        );
+
+        emit newEventCreated(
+            eventId,
+            msg.sender,
+            eventTimestamp,
+            maxCapacity,
+            deposit
+        );
     }
 
-    function createNewRSVP(uint256 eventId) public payable {
-        
+    function createNewRSVP(bytes32 eventId) public payable {
         // look up event
-        Event myEvent = idToEvent[eventId];
+        CreateEvent memory myEvent = idToEvent[eventId];
 
-        //require that they sent in enough ETH
+        // transfer deposit to our contract / require that they sent in enough ETH
         require(msg.value == myEvent.deposit, "NOT ENOUGH");
 
         //require that the event hasn't already happened (<eventTimestamp)
         require(block.timestamp <= myEvent.eventTimestamp, "ALREADY HAPPENED");
 
-        //you'll need to keep track of how many folks have RSVPED 
         //make sure event is under max capacity
-        require(myEvent.confirmedRSVPs.length < myEvent.maxCapacity, "This event has reached capacity"); 
+        require(
+            myEvent.confirmedRSVPs.length < myEvent.maxCapacity,
+            "This event has reached capacity"
+        );
 
         //require that msg.sender isn't already in myEvent.confirmedRSVPs
+        // is there an array.contains() method?
+        for (uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
+            require(myEvent.confirmedRSVPs[i] != msg.sender);
+        }
 
+        //we'll need to keep track of how many folks have RSVPED
+        // this won't work ? because myEvent is stored in memory ?
         myEvent.confirmedRSVPs.push[msg.sender]; //add user to list of rspvs
-
-
-        // transfer deposit to our contract 
-        // myEvent.deposit
-        // use mint functions below
-        // transfer ticket cost to event owner
-        // myEvent.costPerAttendee ==> myEvent.eventOwner
-
-        // you probably just call _mint here
 
         emit newRSVP(eventId, msg.sender);
     }
 
-    function confirmGroup(uint256[] eventIds, address[] attendees) public {
-        require(eventIds.length == attendees.length, "YUIKES");
+    function confirmGroup(bytes32 eventId, address[] calldata attendees) public {
+        // look up event
+        CreateEvent memory myEvent = idToEvent[eventId];
 
-        for(uint8 i=0;i<eventIds.length;i++){
-            confirmAttendee(eventIds[i], attendees[i]);
+        // make sure you require that msg.sender is the owner of the event
+        require(msg.sender == myEvent.eventOwner);
+
+        //confirm each attendee
+        for (uint8 i = 0; i < attendees.length; i++) {
+            confirmAttendee(eventId, attendees[i]);
         }
     }
 
-    function confirmAttendee(uint256 eventId, address attendee) public {
+    function confirmAttendee(bytes32 eventId, address attendee) public {
         // look up event
-        Event myEvent = idToEvent[eventId];
+        CreateEvent memory myEvent = idToEvent[eventId];
 
-
-        // make sure you require that msg.sender is the owner of the event 
-
-        // transfer deposit from our contract back to attendee
-        // myEvent.deposit
-        // attendee
-
-        // sending eth back to the staker https://solidity-by-example.org/sending-ether
-        (bool sent, bytes memory data) = attendee.call{value: the_amount_they_staked}("");
-        require(sent, "Failed to send Ether");
-        
-        //we need to flip some bool so they can't be confirmed twice 
-        // maybe keep a second list of "confirmed" and you check they aren't in that list and then add them 
+        // make sure you require that msg.sender is the owner of the event
+        require(msg.sender == myEvent.eventOwner);
 
         // require that attendee is NOT in the claimedRSVPs list
+        // is there an array.contains() method?
+        for (uint8 i = 0; i < myEvent.claimedRSVPs.length; i++) {
+            require(myEvent.claimedRSVPs[i] != msg.sender);
+        }
 
         // add them to the claimedRSVPs list
+        // this wont work ?
         myEvent.claimedRSVPs.push(attendee);
+
+        // sending eth back to the staker https://solidity-by-example.org/sending-ether
+        (bool sent, bytes memory data) = attendee.call{value: myEvent.deposit}(
+            ""
+        );
+        require(sent, "Failed to send Ether");
+        //what happens if this fails?
 
         emit newRSVP(eventId, msg.sender);
     }
 
-    function withdrawUnclaimedDeposits(uint256 eventId) public {
-
+    function withdrawUnclaimedDeposits(bytes32 eventId) public {
         // look up event
-        Event myEvent = idToEvent[eventId];
+        CreateEvent memory myEvent = idToEvent[eventId];
 
+        // check if already paid
         require(!myEvent.paidOut, "ALREADY PAID");
 
+        // check if it's been 7 days past myEvent.eventTimestamp
+        require(
+            block.timestamp >= (myEvent.eventTimestamp + 7 days),
+            "TOO EARLY"
+        );
+
+        // only the event owner can withdraw
+        require(msg.sender == myEvent.eventOwner, "MUST BE EVENT OWNER");
+
+        // mark as paid
         myEvent.paidOut = true;
 
-        // check if it's been 7 days past myEvent.eventTimestamp
-
-        // if yes, allow event owner to withdraw unclaimed deposits
-        // require msg.sender === myEvent.eventOwner
-
-        // calculate how many people didn't claim by comparing 
-        uint8 unclaimed = myEvent.confirmedRSVPs.length - myEvent.claimedRSVPs.length;
+        // calculate how many people didn't claim by comparing
+        uint256 unclaimed = myEvent.confirmedRSVPs.length -
+            myEvent.claimedRSVPs.length;
 
         uint256 payout = unclaimed * myEvent.deposit;
 
-        // send the payout to the owner 
+        // send the payout to the owner
         (bool sent, ) = msg.sender.call{value: payout}("");
         require(sent, "Failed to send Ether");
-
-    }
-
-    function setSaleState(bool newState) public onlyOwner {
-        _isSaleActive = newState;
-    }
-
-    function setBaseURI(string memory URI) external onlyOwner {
-        baseURI = URI;
-    }    
-
-    function mint(uint8 _numTokens)
-        external
-        payable
-        mintCompliance(_numTokens, MAX_PUBLIC_PURCHASE)
-    {
-        require(_isSaleActive, 'sale inactive');
-        require(msg.value >= (costPerAttendee + deposit) * _numTokens, 'insufficient funds');
-        _mintLoop(msg.sender, _numTokens);
-    }
-
-    function _mintLoop(address _receiver, uint8 _numTokens) internal {
-        for (uint256 i = 0; i < _numTokens; i++) {
-            _tokenIdCounter.increment();
-            _safeMint(_receiver, _tokenIdCounter.current());
-        }
-    }
-
-    modifier mintCompliance(uint256 _numTokens, uint256 _maxPurchase) {
-        require(
-            _numTokens > 0 && _numTokens <= maxCapacity,
-            'invalid mint number'
-        );
-        require(
-            _tokenIdCounter.current() + _numTokens <= maxCapacity,
-            'not enough spots left'
-        );
-	
-        _;
+        // what happens if this fails?
     }
 }
