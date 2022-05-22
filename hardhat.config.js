09980bcee2e0a32d6809abd3e19504f5bd2dea4e
Sarah Schwartz <sarah.schwartz93@gmail.com>
2022-05-22 11:57:02 -0700

    deploy to mumbai testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,15 +4,15 @@ require("hardhat-tracer");
 
 module.exports = {
   solidity: "0.8.4",
-  defaultNetwork: "goerli",
   networks: {
-    hardhat:{},
-    goerli:{
-      url: process.env.STAGING_ALCHEMY_KEY,
+    hardhat:{
+      chainId: 1337
+    },
+    mumbai: {
+      url: process.env.STAGING_INFURA_URL,
       accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
       gas: 2100000,
-      gasPrice: 8000000000,
-      saveDeployments: true,
+      gasPrice: 8000000000
     }
   }
 };
